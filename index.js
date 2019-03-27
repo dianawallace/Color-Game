fb61664887a4c4655e71de9f9a2f90381d56bfa0
Diana Wallace <dianawallacedeveloper@gmail.com>
2019-03-27 13:11:56 +0000

    added js file and variable names

---
+++ index.js
@@ -0,0 +1,11 @@
+
+let numSquares = 6;
+let colors = [];
+let pickedColor;
+
+let squares = document.querySelectorAll(".square");
+let colorDisplay = document.getElementById("colorDisplay");
+let messageDisplay = document.querySelector("#message");
+let h1 = document.querySelector("h1");
+let resetButton = document.querySelector("#reset");
+let modeButtons = document.querySelectorAll(".mode");
